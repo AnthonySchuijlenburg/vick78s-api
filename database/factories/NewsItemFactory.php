bdd50552c255f3182c2a2cfc3139a9b13ad865bf
AnthonySchuijlenburg <a.schuijlenburg@gmail.com>
2024-07-25 13:59:12 +0200

    refactor: replaced FQN's with imports

--- database/factories/NewsItemFactory.php
+++ database/factories/NewsItemFactory.php
@@ -2,11 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\NewsItem;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\NewsItem>
+ * @extends Factory<NewsItem>
  */
 class NewsItemFactory extends Factory
 {
--- database/factories/SponsorFactory.php
+++ database/factories/SponsorFactory.php
@@ -2,10 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\sponsor;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\sponsor>
+ * @extends Factory<sponsor>
  */
 class SponsorFactory extends Factory
 {
