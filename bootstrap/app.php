4cace85b9947c3c1e11fefa809099a0a7553192e
AnthonySchuijlenburg <a.schuijlenburg@gmail.com>
2024-08-16 11:27:34 +0200

    feat: add sentry

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use LaravelJsonApi\Exceptions\ExceptionParser;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -19,5 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->render(
             ExceptionParser::renderer(),
         );
+
+        Integration::handles($exceptions);
     })
     ->create();
