a5039b4116647e28adb3f72ee2d356d749a07ff4
Anthony Schuijlenburg <a.schuijlenburg@gmail.com>
2024-12-22 12:23:04 +0100

    feat: add backing up to r2

--- config/backup.php
+++ config/backup.php
@@ -7,7 +7,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'laravel-backup'),
+        'name' => env('APP_NAME', 'laravel-backup').':'.env('APP_ENV', 'local'),
 
         'source' => [
             'files' => [
@@ -152,6 +152,7 @@ return [
              */
             'disks' => [
                 'local',
+                'r2'
             ],
         ],
 
